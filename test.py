8ed322c03833079063e7816a7fe2836a58e872b5
YessineJallouli <84145572+YessineJallouli@users.noreply.github.com>
2022-01-26 23:39:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,53 +1,88 @@
-from matplotlib import image
-import numpy as np
+# Created by Yessine Jallouli
 
+from matplotlib import image
 
 class RubiksCube():
-    F = np.ndarray(shape=(3, 3), dtype=tuple)
-    B = np.ndarray(shape=(3, 3), dtype=tuple)
-    U = np.ndarray(shape=(3, 3), dtype=tuple)
-    D = np.ndarray(shape=(3, 3), dtype=tuple)
-    R = np.ndarray(shape=(3, 3), dtype=tuple)
-    L = np.ndarray(shape=(3, 3), dtype=tuple)
+    Unknown_Face = [['U' for x in range(3)] for y in range(3)]
+    F = B = U = D = R = L = Unknown_Face
 
+    front = image.imread("front.jpg")  # front should have white color in the center
+    back = image.imread("back.jpg")    # back should have yellow color in the center    Y
+    up = image.imread("up.jpg")        # up should have orange color in the center      O
+    down = image.imread("down.jpg")    # down should have red color in the center     G W B
+    right = image.imread("right.jpg")  # right should have blue color in the center     R
+    left = image.imread("left.jpg")    # left should have green color in the center
 
-    front = image.imread("front.jpg")  # front with white color in the center
-    back = image.imread("back.jpg")    # back with yellow color in the center    Y
-    up = image.imread("up.jpg")        # up with orange color in the center      O
-    down = image.imread("down.jpg")    # down with red color in the center     G W B
-    right = image.imread("right.jpg")  # right with blue color in the center     R
-    left = image.imread("left.jpg")    # left with green color in the center
+    validCube = True
 
     def RGB_to_color(color):
-        return 0
+        red = color[0]
+        green = color[1]
+        blue = color[2]
+        if (red > 230) and (green > 230) and (blue > 230):
+            return 'W'
+        if (red > 230) and (green > 100) and (green < 180) and (blue < 110):
+            return 'O'
+        if (red > 230) and (green > 240) and (blue < 100):
+            return 'Y'
+        if (red < 110) and (green > 170) and (blue < 110):
+            return 'G'
+        if (red < 110) and (green < 110) and (blue > 230):
+            return 'B'
+        if (red > 230) and (green < 110) and (blue < 110):
+            return 'R'
+        return 'U'
 
-    def identifyColors(img):
+    def identifyColors(img, self):
         H = img.shape[0]
         W = img.shape[1]
         height = H // 6
-        colors = np.ndarray(shape=(3, 3), dtype=tuple)
+        colors = [['U' for x in range(3)] for y in range(3)]
         for i in range(3):
             width = W // 6
             for j in range(3):
-                colors[i][j] = img[height][width]
+                colors[i][j] = self.RGB_to_color(img[height][width])
+                if (colors[i][j] == 'U'):
+                    self.validCube = False
                 width += W // 3
             height += H // 3
         return colors
 
     def getColors(self):
-        self.F = self.identifyColors(self.front)
-        self.B = self.identifyColors(self.back)
-        self.U = self.identifyColors(self.up)
-        self.D = self.identifyColors(self.down)
-        self.R = self.identifyColors(self.right)
-        self.L = self.identifyColors(self.left)
+        self.F = self.identifyColors(self.front, self)
+        self.B = self.identifyColors(self.back, self)
+        self.U = self.identifyColors(self.up, self)
+        self.D = self.identifyColors(self.down, self)
+        self.R = self.identifyColors(self.right, self)
+        self.L = self.identifyColors(self.left, self)
+
+    def display_face(self, name, face):
+        print(name, "face :")
+        for i in range(3):
+            for j in range(3):
+                print(face[i][j], end='')
+            print()
+        print()
+
+    def display_all_faces(self):
+        self.display_face(self, "Front", self.F)
+        self.display_face(self, "Back", self.B)
+        self.display_face(self, "Up", self.U)
+        self.display_face(self, "Down", self.D)
+        self.display_face(self, "Right", self.R)
+        self.display_face(self, "Left", self.L)
+
+    #def movR(self):
+        
+
 
 def main():
     cube = RubiksCube
     cube.getColors(cube)
-    for i in range(3):
-        for j in range(3):
-            print(cube.F[i][j], end='')
-        print()
+    if (cube.validCube == False):
+        print("Invalid Rubiks Cube")
+        return 0
+
+    cube.display_all_faces(cube)
 
 main()
